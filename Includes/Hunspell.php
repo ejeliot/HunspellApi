746462a6e596592687c693200a18a04bf379039f
Ed Eliot <me@ejeliot.com>
2014-02-06 22:25:57 +0000

    tidy up

--- Includes/Hunspell.php
+++ Includes/Hunspell.php
@@ -1,15 +1,29 @@
 <?php
+/**
+ * Class Hunspell
+ */
 class Hunspell {
+    /**
+     * @var string
+     */
     protected $cmd = 'echo %s | /usr/bin/hunspell';
 
+    /**
+     * @param string $input
+     * @return array
+     */
     public function check($input) {
-        exec(sprintf($this->cmd, escapeshellarg($input)), $output);
-        return $this->process($output);
+        exec(sprintf($this->cmd, escapeshellarg($input)), $lines);
+        return $this->process($lines);
     }
 
-    protected function process($output) {
+    /**
+     * @param array $lines
+     * @return array
+     */
+    protected function process(array $lines) {
         $results = [];
-        foreach (array_slice($output, 1) as $line) {
+        foreach (array_slice($lines, 1) as $line) {
             if (preg_match('/^\&\s+([^\s]+)[0-9\s]+\:(.+)/', trim($line), $matches)) {
                 $suggestions = explode(',', $matches[2]);
                 $suggestions = array_map('trim', $suggestions);
